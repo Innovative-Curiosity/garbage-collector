0fc6bc99bd913b4a652dbdf283c06d77a4ca0b71
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-05-21 09:10:09 -0400

    Initial commit

---
+++ rust/target/debug/deps/rust-3e30ec0536239abc.d
@@ -0,0 +1,5 @@
+/Users/benlandrette/ccode/multi-language-practice/more-advanced/garbage-collector/rust/target/debug/deps/librust-3e30ec0536239abc.rmeta: src/main.rs
+
+/Users/benlandrette/ccode/multi-language-practice/more-advanced/garbage-collector/rust/target/debug/deps/rust-3e30ec0536239abc.d: src/main.rs
+
+src/main.rs:
---
+++ rust/target/debug/deps/rust-5c3a4b8fb2d1bf8c.d
@@ -0,0 +1,5 @@
+/Users/benlandrette/ccode/multi-language-practice/more-advanced/garbage-collector/rust/target/debug/deps/librust-5c3a4b8fb2d1bf8c.rmeta: src/main.rs
+
+/Users/benlandrette/ccode/multi-language-practice/more-advanced/garbage-collector/rust/target/debug/deps/rust-5c3a4b8fb2d1bf8c.d: src/main.rs
+
+src/main.rs:
